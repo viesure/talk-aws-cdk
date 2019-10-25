6b8e383cb16033fc33e5b73ff538949d5da552b8
Elias Dräxler <elias.draexler@viesure.io>
2019-10-25 10:46:58 +0200

    Added DynamoDB

--- cdk-example/lib/cdk-example-stack.ts
+++ cdk-example/lib/cdk-example-stack.ts
@@ -1,16 +1,28 @@
 import cdk = require('@aws-cdk/core');
 import lambda = require("@aws-cdk/aws-lambda");
-
+import dynamodb = require("@aws-cdk/aws-dynamodb");
+import {RemovalPolicy} from "@aws-cdk/core";
 
 export class CdkExampleStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const storageFunction = new lambda.Function(this, 'cool-function', {
+        const table = new dynamodb.Table(this, 'storage-table', {
+            tableName: 'STORAGE',
+            partitionKey: {name: 'TIMESTAMP', type: dynamodb.AttributeType.STRING},
+            stream: dynamodb.StreamViewType.NEW_IMAGE,
+            removalPolicy: RemovalPolicy.DESTROY
+        });
+
+        const storageFunction = new lambda.Function(this, 'storage-function', {
             runtime: lambda.Runtime.NODEJS_8_10,
             handler: "storage.handler",
-            code: lambda.Code.fromAsset("./lambda")
+            code: lambda.Code.fromAsset("./lambda"),
+            environment: {
+                TABLE_NAME: table.tableName
+            }
         });
+
 
     }
 }
