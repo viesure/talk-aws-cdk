9df8d28b2e2c64a37074f80c0d19017fc0e7633b
Elias Dräxler <elias.draexler@viesure.io>
2019-10-25 15:25:54 +0200

    Added legacy-storage service & fargate task

---
+++ cdk-example/legacy-storage/src/test/kotlin/io/viesure/legacystorage/LegacyStorageApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.viesure.legacystorage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LegacyStorageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
